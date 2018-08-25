b6b815c626f58f2e0ff22342a48023a54c2009b5
ShaoJenChen <cash3310@outlook.com>
2018-08-25 18:25:03 +0800

    Initial Commit

---
+++ MR_Operation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MR_Operation
+//
+//  Created by 陳劭任 on 2018/8/25.
+//  Copyright © 2018年 陳劭任. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
